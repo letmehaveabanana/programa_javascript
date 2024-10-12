9e6b10180250ed634d57d27c042f5a50b997d61d
letmehaveabanana <guilife08@gmail.com>
2024-10-12 11:03:22 +0100

    aula 2

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-document.write("Pontos: ");
+document.write("A pessoa é: ");
 
-let pontos = 10
-document.write(pontos);
-if (pontos >= 1)
-    pontos = pontos + 20
-console.log("Ganhaste")
+let idade = parseInt(prompt("Qual é a sua idade?"));
 
+console.log(idade);
+
+if (idade > 0 && idade <= 12) {
+    console.log("A pessoa é uma criança.");
+}
+else if (idade > 12 && idade <= 18) {
+    console.log("A pessoa é um adolecente.");
+}
+else if (idade > 18 && idade <= 60) {
+    console.log("A pessoa é um adulto.");
+}
+else {
+    console.log("A pessoa é um idoso.");
+}
